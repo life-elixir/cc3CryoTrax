c8841a5543f3f208a6d3682b1b0fe5c0a07bd3ca
zukangy <#x@n@XzJrhLdRh7Uv##t>
2020-12-14 12:23:43 -0800

    Convert to json

---
+++ utils/write_excel_to_json.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import json
+
+
+def convert_excel_to_json(filename, json_file_name, sheet_name=0):
+    df = pd.read_excel('../data/' + filename,
+                       sheet_name=sheet_name,
+                       engine='openpyxl')
+    json_str = df.to_json()
+    with open('../data/' + json_file_name, 'w') as f:
+        json.dump(json_str, f)
+    return
+
+
+if __name__ == '__main__':
+    filepath = 'cryotrack_c3_result_4_days_20201014.xlsx'
+    json_name = 'c3_json.json'
+    sheet = 1
+    convert_excel_to_json(filename=filepath,
+                          json_file_name=json_name,
+                          sheet_name=sheet)
